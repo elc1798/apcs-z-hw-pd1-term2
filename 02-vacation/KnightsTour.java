1f0302e6dd791abd5f7b16162c5be575bcdbbd79
elc1798 <subliminalmau5@gmail.com>
2015-02-17 16:59:01 -0500

    ASSDFASDL:JA

--- 02-vacation/KnightsTour.java
+++ 02-vacation/KnightsTour.java
@@ -15,43 +15,46 @@ public class KnightsTour {
 
 	//Initiates first step
 	public void setup(int r , int c) {
-		for (int[] ary : board) {
-			for (int i : ary) {
-				i = -1;
-			}
-		}
+		for (int asdf = 0; asdf < board.length; asdf++) {
+            for (int qwerty = 0; qwerty < board[asdf].length; qwerty++) {
+                board[asdf][qwerty] = -1;
+            }
+        }
 
-		board[r][c] = 0;
+//		board[r][c] = 0;
         System.out.println("Set board");
 		knightX = r;
 		knightY = c;
 		knightChar = 0;
 	}
 
-	public void step(int r , int c) {
+	public void step_no_heuristic(int r , int c) {
 		if (board[r][c] != -1 || HWUTIL.checkKnightsTour(board)) {
 			return;
 		}
-		HWUTIL.sleep(2000);
+		HWUTIL.sleep(500);
 		fancyPrint();
 		board[r][c] = knightChar;
 		knightX = r;
 		knightY = c;
-		step(r + 1 , c);
-		step(r - 1 , c);
-		step(r , c + 1);
-		step(r , c - 1);
+		try {step_no_heuristic(r + 2 , c + 1);} catch(Exception e){}
+		try {step_no_heuristic(r + 2 , c - 1);} catch(Exception e){}
+		try {step_no_heuristic(r - 2 , c + 1);} catch(Exception e){}
+		try {step_no_heuristic(r - 2 , c - 1);} catch(Exception e){}
+		try {step_no_heuristic(r + 1 , c + 2);} catch(Exception e){}
+		try {step_no_heuristic(r + 1 , c - 2);} catch(Exception e){}
+		try {step_no_heuristic(r - 1 , c + 2);} catch(Exception e){}
+		try {step_no_heuristic(r - 1 , c - 2);} catch(Exception e){}
 		if (!HWUTIL.checkKnightsTour(board)) {
 			board[r][c] = knightChar;
-			knightChar++;
 		}
 	}
 
 	public void fancyPrint() {
 	    HWUTIL.clear();
-        for (int[] ary : board) {
-            for (int i : ary) {
-                System.out.printf("| %dd " , i);
+        for (int asdf = 0; asdf < board.length; asdf++) {
+            for (int qwerty = 0; qwerty < board[asdf].length; qwerty++) {
+                System.out.printf("| %2d " , board[asdf][qwerty]);
             }
             System.out.printf("|\n");
         }
@@ -60,9 +63,15 @@ public class KnightsTour {
     public static void main(String[] args) {
         KnightsTour test = new KnightsTour(5);
         test.setup(0 , 0);
+        test.fancyPrint();
         System.out.println("Starting solver...");
-        test.step(0 , 1);
+        test.step_no_heuristic(0 , 1);
         System.out.println("========== END ==========");
+        test = null;
+        test = new KnightsTour();
+        test.setup(0 , 0);
+        test.fancyPrint();
+        test.step_no_heuristic(0 , 1);
     }
 
 }
