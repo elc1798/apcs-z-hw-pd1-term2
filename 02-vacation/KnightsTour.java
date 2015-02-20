831f8ac39721afa71a9a8f363ffee8dfaba3a04e
elc1798 <subliminalmau5@gmail.com>
2015-02-20 16:08:59 -0500

    Update

--- 02-vacation/KnightsTour.java
+++ 02-vacation/KnightsTour.java
@@ -38,7 +38,7 @@ public class KnightsTour {
 
 	public void solve(int r , int c) {
 		if (nummoves >= board.length * board.length) { // Board is filled
-			System.exit(0);
+			return;
 		} else if (r < 0 || c < 0 || r >= board.length || c >= board.length) { // Out of bounds
 			return;
 		} else if (board[r][c] != blank) { // Already visited
@@ -91,13 +91,26 @@ public class KnightsTour {
         useHeuristic = true;
     }
 
+    public void cycleThrough() {
+        for (int i = 0; i < board.length && nummoves < board.length * board.length; i++) {
+            fillBlank();
+            solve(i , 0);
+            nummoves = 0;
+        }
+        if (nummoves < board.length * board.length) {
+            System.out.println("No solution found.");
+        }
+    }
+
 	public static void main(String[] args) {
-		KnightsTour k = new KnightsTour(5); // Try 5 :)
-        k.enableHeuristics();
-        k.solve(0 , 0);
-		// k = null;
-		// k = new KnightsTour(); // Try 8 :)
-		// k.solve(0 , 0);
+		KnightsTour k;
+        for (int i = 1; i < 10; i++) {
+            k = new KnightsTour(i);
+            k.enableHeuristics();
+            k.cycleThrough();
+            HWUTIL.sleep(3000);
+            k = null;
+        }
 	}
 
 }
