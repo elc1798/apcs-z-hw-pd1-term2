de0e35949c220f479b3c96e55185785cf02eeead
elc1798 <subliminalmau5@gmail.com>
2015-02-24 00:40:55 -0500

    Solve MakeLake

--- 03-usaco/mar08-bronze/LakeMaking.java
+++ 03-usaco/mar08-bronze/LakeMaking.java
@@ -12,8 +12,7 @@ public class LakeMaking {
 
     public static int[][] lake;
     public static int LIMIT;
-    public static int[][][] diggydiggyhole;
-    public static int[] down;
+    public static int down;
     public static int answer = 0;
 
     public static void readFromFile(File fin) throws IOException {
@@ -34,61 +33,59 @@ public class LakeMaking {
             }
         }
 
-        diggydiggyhole = new int[N][lake.length][lake[0].length];
-        down = new int[N];
-
-        for (int i = 0; i < diggydiggyhole.length; i++) {
-            for (int k = 0; k < diggydiggyhole[i].length; k++) {
-                Arrays.fill(diggydiggyhole[i][k] , 0);
-            }
-        }
-
         for (int i = 0; i < N; i++) {
             line = br.readLine().split("\\s+");
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 3; k++) {
-                    try { diggydiggyhole[i][Integer.parseInt(line[0]) + j][Integer.parseInt(line[1]) + k] += Integer.parseInt(line[2]); } catch(Exception e) {}
-                }
-            }
-            down[i] = Integer.parseInt(line[2]);
-        }
+            System.out.println("INSTRUCTION: " + line[0] + " " + line[1] + " " + line[2]);
+            down = Integer.parseInt(line[2]);
+            dig(new int[]{Integer.parseInt(line[0]) , Integer.parseInt(line[1])} , down);
+        }   
 
         br.close();
-
-        // Verify
-        
-        System.out.println(LIMIT);
-
     }
 
-    public static void dig(int[][] target , int d) {
+    public static void dig(int[] target , int d) {
         // Enumerate which ones to decrease
         
         int currmax = 0;
+        int x = target[0] - 1;
+        int y = target[1] - 1;
+        
+        System.out.println();
+        for (int i = 0; i < lake.length; i++) {
+            System.out.println(Arrays.toString(lake[i]));
+        }
 
-        for (int i = 0; i < target.length; i++) {
-            for (int k = 0; k < target[i].length; k++) {
-                if (lake[i][k] > currmax) {
-                    currmax = lake[i][k];
-                }
+        for (int i = 0; i < 3; i++) {
+            for (int k = 0; k < 3; k++) {
+                try {
+                    if (lake[x + i][y + k] > currmax) {
+                        currmax = lake[x + i][y + k];
+                    }
+                } catch(Exception e) {}
             }
         }
 
-        for (int i = 0; i < target.length; i++) {
-            if (d <= 0) { break; }
-            for (int k = 0; k < target[i].length; k++) {
-                if (lake[i][k] == currmax) {
-                    currmax--;
-                    lake[i][k] -= 1;
-                    d--;
+        while (d > 0) {
+            for (int i = 0; i < 3; i++) {
+                for (int k = 0; k < 3; k++) {
+                    try {
+                        if (lake[x + i][y + k] == currmax) {
+                            lake[x + i][y + k] -= 1;
+                        }
+                    } catch(Exception e) {}
                 }
             }
-//            if (d <= 0) { break; }
+            currmax--;
+            d--;
         }
-
     }
 
     public static void invert() {
+
+        for (int i = 0; i < lake.length; i++) {
+            System.out.println(Arrays.toString(lake[i]));
+        }
+
         for (int i = 0; i < lake.length; i++) {
             for (int k = 0; k < lake[i].length; k++) {
                 lake[i][k] = LIMIT - lake[i][k];
@@ -97,12 +94,15 @@ public class LakeMaking {
                 }
             }
         }
+
+        System.out.println();
+
+        for (int i = 0; i < lake.length; i++) {
+            System.out.println(Arrays.toString(lake[i]));
+        }
     }
 
     public static void process() {
-        for (int i = 0; i < diggydiggyhole.length; i++) {
-            dig(diggydiggyhole[i] , down[i]);
-        }
         invert();
     }
 
