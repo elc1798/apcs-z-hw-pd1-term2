f3f1d7d0103c61bd45d4e46b92c0a7f444cb86f3
elc1798 <subliminalmau5@gmail.com>
2015-03-07 16:42:18 -0500

    Remove incorrect programs

--- 05-selection/Driver.java
+++
@@ -1,9 +0,0 @@
-public class Driver {
-    public static void main(String[] args) {
-        int[] test_1 = new int[]{0 , 1 , 4 , 9 , 2 , 8 , 7 , 3 , 6 , 5};
-        QuickArray loader = new QuickArray(test_1);
-        for (int i = -1 ; i < 11 ; i++) {
-            System.out.println(loader.getNthSmallest(i));
-        }
-    }
-}
