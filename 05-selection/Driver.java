f34ac86d95ecfde7c7f58c5a56f7264d8136b24c
elc1798 <subliminalmau5@gmail.com>
2015-03-08 19:42:20 -0400

    Add quickselect

---
+++ 05-selection/Driver.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Arrays;
+
+public class Driver {
+
+    private static final int DEFAULT_STRESS_TEST_ARRAY_SIZE = 15;
+    private static final Random r = new Random();
+
+    public static void main(String[] args) {
+        int[] test_1 = new int[]{9 , 1 , 0 , 2 , 5 , 6 , 3 , 8 , 7 , 4};
+        int[] test_2 = new int[]{};
+        if (args.length > 0) {
+            try {
+                test_2 = new int[Integer.parseInt(args[0])];
+            } catch(Exception e) {}
+        } else {
+            test_2 = new int[DEFAULT_STRESS_TEST_ARRAY_SIZE];
+        }
+
+        for (int i = 0 ; i < test_2.length ; i++) {
+            test_2[i] = r.nextInt(test_2.length);
+        }
+
+        System.out.println("Basic list test: ");
+        for (int i = -1 ; i < test_1.length + 1 ; i++) {
+            System.out.println(QuickSelect.select(test_1 , i));
+        }
+
+        System.out.println("Stress test: ");
+        System.out.println(Arrays.toString(test_2));
+        for (int i = 0 ; i < test_2.length ; i++) {
+            System.out.println(QuickSelect.select(test_2 , i));
+        }
+    }
+
+}
---
+++ 05-selection/QuickSelect.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+
+public class QuickSelect {
+
+    /**
+     *
+     * @param a = Array to process
+     * @param k - Kth smallest number in array, domain of [0 , a.length)
+     * @param l - Lower bound index to process
+     * @param h - Higher bound index to process
+     *
+     */
+
+    public static int select(int[] a , int k , int l , int h) {
+        if (h >= a.length) {
+            h = a.length - 1;
+        }
+        if (k < 0 || k >= a.length) {
+            return -1;
+        } else if (l >= h) {
+            return a[k];
+        } else {
+            int[] b = new int[a.length];
+            int pivotIndex = (int)(l + (h - l) / 2);
+            int pivot = a[pivotIndex];
+            int lower_bound = l;
+            int higher_bound = h;
+            for (int i = l; i <= h; i++) {
+                if (a[i] < pivot) {
+                    b[higher_bound] = a[i];
+                    higher_bound--;
+                } else if (a[i] > pivot) {
+                    b[lower_bound] = a[i];
+                    lower_bound++;
+                }
+            }
+            pivotIndex = lower_bound;
+            for (int i = lower_bound; i <= higher_bound; i++) {
+                b[i] = pivot;
+            }
+            if (b[k] == pivot) {
+                return pivot;
+            } else {
+                if (lower_bound > k) {
+                    return select(b , k , l , pivotIndex - 1);
+                } else {
+                    return select(b , k , pivotIndex + 1 , h);
+                }
+            }
+        }
+    }
+
+    public static int select(int[] a , int k) {
+        return select(a , a.length - k - 1 , 0 , a.length - 1);
+    }
+
+}
