54b96905dd823fe386c93c2eb947dd7e9ce671ce
elc1798 <subliminalmau5@gmail.com>
2015-03-09 23:43:07 -0400

    Finish QuickSelect

---
+++ 06-QuickSort/Driver.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+import java.util.Arrays;
+
+public class Driver {
+
+    private static final int DEFAULT_STRESS_TEST_ARRAY_SIZE = 15;
+    private static final Random r = new Random();
+
+    public static void main(String[] args) {
+        int[] test_1 = new int[]{9 , 1 , 0 , 2 , 5 , 6 , 3 , 8 , 7 , 4};
+        int[] test_2 = new int[]{};
+        if (args.length > 0) {
+            try {
+                test_2 = new int[Integer.parseInt(args[0])];
+            } catch(Exception e) {}
+        } else {
+            test_2 = new int[DEFAULT_STRESS_TEST_ARRAY_SIZE];
+        }
+
+        for (int i = 0 ; i < test_2.length ; i++) {
+            test_2[i] = r.nextInt(test_2.length);
+        }
+
+        System.out.println("Basic list test: ");
+        QuickSort.qsort(test_1);
+        System.out.println(Arrays.toString(test_1));
+
+        System.out.println("Stress test: ");
+        System.out.println(Arrays.toString(test_2));
+
+        System.out.println("Stress test solution: ");
+        int[] test_2_solution = new int[test_2.length];
+        System.arraycopy(test_2 , 0 , test_2_solution , 0 , test_2.length);
+        Arrays.sort(test_2_solution);
+        System.out.println(Arrays.toString(test_2_solution));
+
+        System.out.println("Trial run: ");
+        QuickSort.qsort(test_2);
+        System.out.println(Arrays.toString(test_2));
+    }
+
+}
---
+++ 06-QuickSort/QuickSort.java
@@ -0,0 +1,51 @@
+public class QuickSort {
+
+    private static void qsort(int[] a , int l , int h) {
+
+        if (h >= a.length) {
+            return;
+        }
+        if (l < 0) {
+            return;
+        }
+
+        if (l >= h) {
+            return;
+        } else {
+            int[] b = new int[a.length];
+            System.arraycopy(a , 0 , b , 0 , a.length);
+            int pivotIndex = (int)(l + ((h - l) / 2));
+            int pivot = a[pivotIndex];
+            int lower_bound = l;
+            int higher_bound = h;
+            for (int i = l ; i <= h ; i++) {
+                if (a[i] > pivot) {
+                    b[higher_bound] = a[i];
+                    higher_bound--;
+                } else if (a[i] < pivot) {
+                    b[lower_bound] = a[i];
+                    lower_bound++;
+                }
+            }
+            for (int i = Integer.valueOf(lower_bound); i <= higher_bound; i++) {
+                b[i] = pivot;
+            }
+            System.arraycopy(b , 0 , a , 0 , a.length);
+            int k = Integer.valueOf(lower_bound);
+            qsort(a , l , lower_bound - 1);
+            qsort(a , k + 1 , h);
+        }
+    }
+
+    public static void qsort(int[] a) {
+        qsort(a , 0 , a.length - 1);
+    }
+
+    public static int[] qsorted(int[] a) {
+        int[] b = new int[a.length];
+        System.arraycopy(a , 0 , b , 0 , a.length);
+        qsort(b , 0 , a.length - 1);
+        return b;
+    }
+
+}
